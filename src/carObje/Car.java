57d901e54f97f8e2bbe64ad2c1dedffaab5e4ecd
yuhbeTAHA <mitzhutmm@gmail.com>
2021-10-12 15:50:42 +0300

    Initial commit

---
+++ src/carObje/Car.java
@@ -0,0 +1,70 @@
+package carObje;
+
+public class Car {
+
+	// deðiþkenler
+	private String caption;
+	private int price;
+	private String brand;
+	private String sherry;
+	private String model;
+	private int year;
+	private boolean paint;
+
+	// set methodlarý
+	public void setCaption(String caption) {
+		this.caption = caption;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public void setSherry(String sherry) {
+		this.sherry = sherry;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public void setPaint(Boolean painy) {[]
+		this.paint=year;
+	}
+
+	// get methodlarý
+	public String getCaption() {
+		return this.caption;
+	}
+
+	public int getPrice() {
+		return this.price;
+
+	}
+
+	public String getBrand() {
+		return this.brand;
+
+	}
+
+	public String getSerry() {
+		return this.sherry;
+	}
+
+	public String getModel() {
+		return this.model;
+	}
+
+	public int getYear() {
+		return this.year;
+	}
+
+}
